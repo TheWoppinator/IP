209d25775dbdcf5b41cbf65c17ffece99714b601
TheWoppinator <34474631+TheWoppinator@users.noreply.github.com>
2017-12-13 13:35:18 +0100

    With switch

--- main12.cpp
+++ main12.cpp
@@ -6,6 +6,13 @@
 
 using namespace std;
 vector<int> giftsPrice;
+vector<string> andrew;
+vector<string> belle;
+vector<string> chris;
+vector<string> fabienne;
+vector<string> edward;
+vector<string> desiree;
+
 enum Name{Andrew, Belle, Chris, Desiree, Edward, Fabienne, Exit};
 
 vector<string> read_file(string file, vector<string> giftList, bool wishes){
@@ -27,16 +34,13 @@ vector<string> read_file(string file, vector<string> giftList, bool wishes){
         std::istringstream (price_string) >> price;
         giftsPrice.push_back(price);
         }
-        cout << "price: " << price;
         while(a != '\n'){
             name += a;
             a = infile.get();
         }
         a = infile.get();
         giftList.push_back(name);
-        cout << " Name : " << name << endl;
     }
-    cout << "muh" << endl;
     infile.close();
     return giftList;
 
@@ -45,6 +49,7 @@ vector<string> read_file(string file, vector<string> giftList, bool wishes){
 
 void gifts(vector<string> store, vector<int> prices, vector<string> wishes){
 
+    // misschien in de main gooien om mee te geven
     int budget;
     std::istringstream (wishes[0]) >> budget;
 
@@ -55,42 +60,37 @@ int main()
     cout << "making store list..." << endl;
     vector<string> StoreGifts;
     StoreGifts = read_file("giftstore.txt", StoreGifts, false);
-    bool go = false;
+    bool go = true;
     while(go){
     cout << "Please select a name or exit Andrew = 1, Belle = 2, Chris = 3, Desiree = 4, Edward = 5, Fabienne = 6, Exit = 7" << endl;
-    int select;
+    char select;
     cin >> select;
     switch (select){
-        case 1:
-            vector<string> andrew;
+        case '1':
             andrew = read_file("Andrew.txt", andrew, true);
             break;
-        case 2:
-            vector<string> belle;
+        case '2':
             belle = read_file("Belle.txt", belle, true);
             break;
-        case 3:
-            vector<string> chris;
+        case '3':
             chris = read_file("Chris.txt", chris, true);
             break;
-        case 4:
-            vector<string> desiree;
+        case '4':
             desiree = read_file("Desiree.txt", desiree, true);
             break;
-        case 5:
-            vector<string> edward;
+        case '5':
             edward = read_file("Edward.txt", edward, true);
             break;
-        case 6:
-            vector<string> fabienne;
+        case '6':
             fabienne = read_file("Fabienne.txt", fabienne, true);
             break;
-        case 7:
+        case '7':
             go = false;
             break;
-        case default:
+        default:
             cout << "Choose valid number" << endl;
+            break;
+    }
     }
     cout << "Terminating" << endl;
-    }
 }
